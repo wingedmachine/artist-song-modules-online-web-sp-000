1b6cbf3b3839f2837350fcf7f8c8150f76761208
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 03:01:21 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/memorable.rb'
-# require_relative '../lib/concerns/paramable'
+require_relative '../lib/concerns/paramable.rb'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
